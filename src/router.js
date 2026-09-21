9dc9ca5e73fea3fc63b4424ac8c00d5bb4a9017a
Joaquin <joaquinrm1201@gmail.com>
2026-09-21 18:02:54 -0500

    feat(in-app-navigation): configure routes for home, about, and page-not-found-views

---
+++ src/router.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "./shared/presentation/views/home.vue";
+import About from "./shared/presentation/views/about.vue";
+
+const pageNotFound = () => import("./shared/presentation/views/page-not-found.vue");
+
+const routes = [
+    { path: "/home", name: "home", component: Home, meta: { title: "Home" } },
+    { path: "/about", name: "about", component: About, meta: { title: "About" } },
+    { path: "/", redirect: "/home" },
+    {
+        path: "/:pathMatch(.*)*",
+        name: "not-found",
+        component: pageNotFound,
+        meta: { title: "Page Not Found" },
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: routes
+});
+
+export default router;
